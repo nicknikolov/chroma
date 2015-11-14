4ce4e20277ccc008fcb945946a69df0739723a3d
nicknikolov <nick@nklv.me>
2015-11-14 16:17:23 +0000

    Replace socket port from 3000 to 8888

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ io.on('connection', function (socket) {
   })
 })
 
-http.listen(3000, function () {
-  console.log('listening on *:3000')
+http.listen(8888, function () {
+  console.log('listening on *:8888')
 })
 
 
